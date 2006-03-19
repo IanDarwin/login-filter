e110f9c7d0095e632965cfcc89957bc1daca9e9d
IanDarwin <>
2006-03-19 18:58:57 +0000

    Logging, getting username from LoginStuff.getUserName()

--- src/demo/LogoutServlet.java
+++ src/demo/LogoutServlet.java
@@ -9,24 +9,32 @@ import javax.servlet.http.HttpServletResponse;
 
 
 /**
- * This class is the Login/Logout handler.
+ * This class is the Logout handler.
  */
 public class LogoutServlet extends HttpServlet {	
 
 	@Override
-	public void doGet(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		process(arg0, arg1);
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
 	}
 
 	@Override
-	public void doPost(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		process(arg0, arg1);
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
 	}
 
 	protected void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        request.getSession().removeAttribute("LOGIN_FLAG");
+		String name = LoginStuff.getUserName(request);
+		if (name == null) {
+			System.out.printf("Non-logged-in user got to LogoutServlet from %s%n", request.getRemoteAddr());
+		}
+		System.out.printf("Logging out user %s%n", name);
+		
+        request.getSession().removeAttribute(LoginStuff.LOGIN_FLAG);
         
         response.sendRedirect("/");
     }
+
+
 }
