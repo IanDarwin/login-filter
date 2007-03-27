61daaf448d8fbedb82f09f1508c435cf0e802a58
IanDarwin <>
2007-03-27 00:42:44 +0000

    Less verbose.

--- src/demo/LogoutServlet.java
+++ src/demo/LogoutServlet.java
@@ -7,11 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  * This class is the Logout handler.
  */
-public class LogoutServlet extends HttpServlet {	
+public class LogoutServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -2226253641448684463L;
 
@@ -27,14 +26,15 @@ public class LogoutServlet extends HttpServlet {
 
 	protected void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-		String name = LoginStuff.getUserName(request);
+		String name = request.getRemoteUser();
 		if (name == null) {
 			System.out.printf("Non-logged-in user got to LogoutServlet from %s%n", request.getRemoteAddr());
+		} else {
+			System.out.printf("Logging out user %s%n", name);
 		}
-		System.out.printf("Logging out user %s%n", name);
-		
+
         request.getSession().removeAttribute(LoginStuff.LOGIN_FLAG);
-        
+
         response.sendRedirect(".");
     }
 
