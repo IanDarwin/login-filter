a0ad3118d852a75ab5ddfca7bfa4228f5cbbe401
IanDarwin <>
2008-03-27 18:00:14 +0000

    Redirect to /, not .

--- src/demo/LogoutServlet.java
+++ src/demo/LogoutServlet.java
@@ -35,7 +35,7 @@ public class LogoutServlet extends HttpServlet {
 
         request.getSession().removeAttribute(LoginStuff.LOGIN_FLAG);
 
-        response.sendRedirect(".");
+        response.sendRedirect("/");
     }
 
 
